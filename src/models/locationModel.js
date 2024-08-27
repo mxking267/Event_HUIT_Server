834d57216476fa7b76f63a5f02934a22fb0e52c4
mxking <mxking267@gmail.com>
2024-08-27 22:45:33 +0700

    Initial commit

---
+++ src/models/locationModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const locationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+}, { timestamps: true });
+
+const Location = mongoose.model('Location', locationSchema);
+
+module.exports = Location;
