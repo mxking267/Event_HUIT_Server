bbc7bee45d6c6666a57444804ff4e72e6fd98183
mxking <mxking267@gmail.com>
2024-09-18 11:40:40 +0700

    refactor: refactor port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Kết nối MongoDB
 mongoose.connect(process.env.DB_URI)
