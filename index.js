2e4376c728833f4b827000223ef7b69353ce29ea
mxking <mxking267@gmail.com>
2024-10-25 22:08:51 +0700

    feat: handle cors

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ const auth = require('./src/middleware/auth')
 dotenv.config()
 
 const app = express()
-app.use(cors())
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  })
+)
+
+app.options('*', cors())
+
 const port = process.env.PORT || 8080
 
-// Kết nối MongoDB
 mongoose
   .connect(process.env.DB_URI)
   .then(() => console.log('Connected to MongoDB'))
@@ -28,5 +38,5 @@ app.use('/api/v1', eventRoutes)
 app.use('/api/v1', authRoutes)
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port} with cors enable`)
 })
