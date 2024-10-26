d346dcbd876bce77eac670fe54ecb86367b7c05c
mxking <mxking267@gmail.com>
2024-10-26 10:21:13 +0700

    feat: add origin clien

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://huit-client.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
